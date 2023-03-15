11e1972220784163ddc9659512c3c993124e5cac
higorpr <higorpr@gmail.com>
2023-03-15 00:50:37 -0300

    feat: implement error screen when user ticket is remote

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,5 +1,38 @@
-import HotelSummary from '../../../components/HotelSummary';
+import { Typography } from '@material-ui/core';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import NotPresentialTicket from '../../../components/NotPresentialTicket';
+import useGetTicket from '../../../hooks/api/useGetTicket';
 
 export default function Activities() {
-  return 'Em breve!';
+  const [ticketInfo, setTicketInfo] = useState('');
+  const { getTicket } = useGetTicket();
+
+  useEffect(() => {
+    async function getUserTicket() {
+      try {
+        const userTicket = await getTicket();
+        setTicketInfo(userTicket);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getUserTicket();
+  }, []);
+
+  if (ticketInfo) {
+    if (ticketInfo.TicketType.isRemote === true) {
+      return <NotPresentialTicket />;
+    }
+  }
+
+  return (
+    <>
+      <StyledTypography variant="h4">Escolha de Atividades</StyledTypography>
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px !important;
+`;
