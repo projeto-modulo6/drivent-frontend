f0f688a958947b9837390a87f0e3bc5193463960
MarcusCastro0701 <marcus.castro0701@gmail.com>
2023-03-16 10:45:48 -0300

    fix: eslint

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -86,7 +86,6 @@ export default function Hotel() {
         <HotelSummary
           setBookingCompleted={setBookingCompleted}
           setQuery={setQuery}
-          bookingId={bookingId}
           hotelId={hotelId}
         />
       )}
