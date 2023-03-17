3927273b8d64ca949a3b6ab977065353e5f75161
Lucas Macedo <lucasamacedo@hotmail.com>
2023-03-17 14:10:23 -0300

    feat: activities dates buttons

---
+++ src/components/ActivityDay/ButtonDay.js
@@ -0,0 +1,29 @@
+import dayjs from 'dayjs';
+import styled from 'styled-components';
+
+export default function ButtonDay({ dayId, setDayId, getDay, setDaySelected, item }) {
+  function select() {
+    setDaySelected(true);
+    setDayId(item.id);
+  }
+  return (
+    <>
+      <Button boolean={dayId === item.id} onClick={select}>
+        {getDay(dayjs(item.date))}
+      </Button>
+    </>
+  );
+}
+
+const Button = styled.button`
+  cursor: pointer;
+  width: 131px;
+  height: 37px;
+  background: ${(props) => (props.boolean ? '#FFD37D' : '#e0e0e0')};
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+  border: 0px;
+  margin-right: 17px;
+  font-family: 'Roboto';
+  font-size: 14px;
+`;
---
+++ src/components/ActivityDay/index.js
@@ -0,0 +1,83 @@
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import useDateWithActivities from '../../hooks/api/useDateWithActivity';
+import ButtonDay from './ButtonDay';
+
+export default function ActivityDay({ dayId, setDayId }) {
+  const [daySelected, setDaySelected] = useState(false);
+  const { dateWithActivities, dateWithActivitiesLoading, dateWithActivitiesError } = useDateWithActivities();
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    async function getDates() {
+      try {
+        const dates = await dateWithActivities();
+        setActivities(dates);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getDates();
+  }, []);
+
+  function getDay(date) {
+    let weekDay = '';
+    /* eslint-disable */
+    switch (date.day()) {
+      case 0:
+        weekDay = 'Domingo';
+        break;
+      case 1:
+        weekDay = 'Segunda';
+        break;
+      case 2:
+        weekDay = 'Terça';
+        break;
+      case 3:
+        weekDay = 'Quarta';
+        break;
+      case 4:
+        weekDay = 'Quinta';
+        break;
+      case 5:
+        weekDay = 'Sexta';
+        break;
+      case 6:
+        weekDay = 'Sábado';
+        break;
+      default:
+        break;
+    }
+    /* eslint-enable */
+    return weekDay + ', ' + date.format('DD/MM');
+  }
+  return (
+    <>
+      <div>
+        {daySelected ? null : <Text>Primeiro, filtre pelo dia do evento: </Text>}
+        <Box>
+          {activities.map((item) => (
+            <ButtonDay
+              dayId={dayId}
+              setDayId={setDayId}
+              setDaySelected={setDaySelected}
+              getDay={getDay}
+              item={item}
+            ></ButtonDay>
+          ))}
+        </Box>
+      </div>
+    </>
+  );
+}
+
+const Box = styled.div`
+  display: flex;
+`;
+
+const Text = styled.h1`
+  font-size: 20px;
+  font-family: 'Roboto';
+  color: #8e8e8e;
+  margin-bottom: 25px;
+`;
